2c6981fcf80619625b6d89606e3c457d8be04df8
tanveer <tanvirbullet@gmail.com>
2025-04-20 22:13:06 +0600

    section 13 practice3

---
+++ section-13/practice3.js
@@ -0,0 +1,51 @@
+// PRACTICE NUMBER 1
+// const numbers = [5,10,15,20,25];
+// const sumNumbers = numbers.reduce((sum,numbers) => sum + numbers,0);
+// console.log(sumNumbers);
+
+
+
+// PRACTICE NUMBER 2
+// const products = [
+//     {name:'Shampoo',price:100},
+//     {name:'Soap',price:50},
+//     {name:'Toothpaste',price:75},
+// ]
+// const total = products.reduce((sum,num) => sum+num.price,0);
+// console.log(total);
+
+
+
+
+// PRACTICE NUMBER 3
+// const products = [
+//     {name:'pen',price:5},
+//     {name:'book',price:50},
+//     {name:'Bag',price:100}
+// ]
+// const total = products.reduce((sum,products) => sum+products.price,0);
+// console.log(`all product's price is ${total}`);
+
+
+
+
+// PRACTICE NUMBER 4
+// const numbers = [1,2,3,4,5];
+// const sum = numbers.reduce((a,b) => a+b,0);
+// console.log(sum);
+
+
+
+
+// PRACTICE NUMBER 5
+// const numbers = [10,20,30,40,50];
+// const maxnum = numbers.reduce((max,num)=> num>max?num:max,0);
+// console.log(maxnum);
+
+
+
+
+// PRACTICE NUMBER 6
+const numbers = [100,200,300,400];
+const sum = numbers.reduce((a,b) => a+b,0);
+console.log(sum);
