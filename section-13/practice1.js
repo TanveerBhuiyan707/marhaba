3ecf1402cd0880b5c6771028925f20d0f0223dd6
tanveer <tanvirbullet@gmail.com>
2025-04-20 18:32:05 +0600

    section 13, practice2.js

---
+++ section-13/practice1.js
@@ -0,0 +1,101 @@
+// PRACTICE NUMBER 1
+// const item = [30,45,20,60,10];
+// const result = item.map(num => num + 13);
+// console.log(result);
+
+
+
+
+// PRACTICE NUMBER 2
+// const list = ['Messi','Meradona','Pele','Zidane','Ronaldo'];
+// const update = list.filter(name => name.length >5)
+// console.log(update);
+
+
+
+
+
+// PRACTICE NUMBER 3
+// const number = [10,15,20,25,30,35];
+// const findNumber = number.find(num => num>20);
+// console.log(findNumber);
+
+
+
+
+
+// PRACTICE NUMBER 4
+// const height = [65,70,68,72,68,73];
+// const maxHeight = height.filter(num=>num>69);
+// console.log(maxHeight);
+
+
+
+
+// PRACTICE NUMBER 5
+// const numbers = [7,10,15,20,25,30];
+// const divid = numbers.map(num => num/3);
+// const once = divid.forEach(num => console.log(num.toFixed(2)));
+
+
+
+
+
+// PRACTICE NUMBER 6
+// const names = ['Leonardo','Brad Pitt','Kate Winslet','Audrey Hepburn','Johnny Depp']
+// const Thirdword = names.map(num => num[2])
+// console.log(Thirdword);
+
+
+
+
+// PRACTICE NUMBER 7
+// const names = ['Leonardo','Brad Pitt','Kate Winslet','Audrey Hepburn','Johnny Depp'];
+// const findName = names.find(num => num[0]==='B');
+// console.log(findName);
+
+
+
+
+
+// PRACTICE NUMBER 8
+// const arr = [1,2,3,4,5];
+// const resul = arr.forEach(num => console.log(num));
+
+
+
+
+
+// PRACTICE NUMBER 9
+const names = ['cow','goat','sheep','horse'];
+const result = names.forEach(num=> console.log(num))
+
+
+// const numbers= [4,5,2,8,10];
+// const result = numbers.map(num => num * 2)
+// console.log(result);
+
+
+
+// const numbers = [12,10,8,15,7];
+// const fiveBonus = numbers.map(num => num +5);
+// const halves = numbers.map( num => num/2);
+// console.log(fiveBonus);
+// console.log(halves);
+
+
+
+// const friends= ["Tom","Jerry","Oggy","Jack","Oly"];
+// const lengths = friends.map(frnd => frnd.length);
+// const firstLetters = friends.map(fr => fr[0]);
+// console.log(lengths);
+// console.log(firstLetters);
+
+
+
+
+// const numbers = [1,5,6,4,15];
+// const rersult = numbers.forEach(num => num * 2);
+// console.log(rersult);
+
+
---
+++ section-13/practice2.js
@@ -0,0 +1,45 @@
+// PRACTICE NUMBER 1
+// const numbers = [58,99,35,101,88];
+// const findNumber =numbers.some(num => num>100);
+// console.log(findNumber);
+
+
+
+
+// PRACTICE NUMBER 2
+// const numbers = [58,99,35,101,88];
+// const findNumber = numbers.every(num=> num%5===0);
+// console.log(findNumber); // false
+
+
+
+
+
+// PRACTICE NUMBER 3
+// const words = ['hello','world','javascript','is','awesome'];
+// const result = words.some(words => words === 'hello');
+// console.log(result);
+
+
+
+
+// PRACTICE NUMBER 4
+// const ages = [15,22,18,30,25];
+// const findAge = ages.every(age => age > 18);
+// console.log(findAge);
+
+
+// const scores = [30,45,55,80,90];
+// const hasPassed = scores.some(scores => scores >50);
+// console.log(hasPassed);
+
+
+
+// const scores = [30,45,55,80,90];
+// const hasPassed = scores.every(scores => scores >50);
+// console.log(hasPassed);
+
+
+// const nested = [1,2,[3,4,[5,6]]];
+// const flast = nested.flat(2);
+// console.log(flast);
