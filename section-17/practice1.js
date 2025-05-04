c0d3b30960397db3deaf5a5db2ae1545757f65c0
tanveer <tanvirbullet@gmail.com>
2025-05-05 05:37:13 +0600

    section-17 completed

---
+++ section-17/practice1.js
@@ -0,0 +1,78 @@
+// PRACTICE NUMBER 1
+// function names(name){
+//     if(typeof name !=='string'){
+//         return 'Please provide a string';
+//     }
+//     return name[0];
+// }
+// const name = names('Tanveer');
+// console.log(name);
+
+
+
+
+
+// PRACTICE NUMBER 2
+// function arrays(arr){
+//     if(arr ==Array.isArray(false)){
+//         return 'please provide an array';
+//     }
+//     const findElement = arr.length -1;
+//     return findElement
+// }
+// const result = arrays([1,2,3,4,5,6,7]);
+// console.log(result);
+
+
+
+
+// PRACTICE NUMBER 3
+// function getAria(length,width){
+//     if(typeof length !== 'number' && typeof width !== 'number'){
+//         return 'please provide an number';
+//     }
+//     const aria = length *width;
+//     return aria;
+// }
+// console.log(getAria(9,7));
+
+
+// function multiply(num1,num2){
+//     if(typeof num1 !== 'number'|| typeof num2 !=='number'){
+//         return 'Please provide a number'
+//     }
+//     const mult = num1*num2;
+//     return mult;
+// }
+// const result = multiply(5,7)
+// console.log(result);
+
+
+
+// function fullName(name1,name2){
+//     if(typeof name1 !=='string'){
+//         return 'name1 should be a string';
+//     }else if(typeof name2 !=='string'){
+//         return 'name2 should be a string';
+//     }
+//     const name = name1 + ' ' + name2;
+//     return name;
+// }
+// const result = fullName('Tanveer','bhuiyan');
+// console.log(result);
+
+
+// function getPrice(product){
+//     if(typeof product !== 'object'){
+//         return 'Please provide an object';
+//     }
+//     const price = product.price;
+//     return price;
+// }
+// const price = getPrice({name:'jhon',age:32,price:302});
+// console.log(price);
+
+
+
+// const test = [];
+// console.log(Array.isArray(test));
---
+++ section-17/practice2.js
@@ -0,0 +1,14 @@
+// PRACTICE NUMBER 1
+// Semicolon na dile sobsomoy error dey na, abar kichu khetre error dey, 
+// jemon return statements, for loops বা block scopes er khetre;
+// tai best practice holo SEMICOLON USE KORA
+
+
+
+// PRACTICE NUMBER 2
+// REFERENCE ERROR tokhon dibe jokhon ami kono kichu difine ba declare na kore output er try kori
+
+
+
+// PRACTICE NUMBER 3
+// syntex error holo ja amr code likhte giye vul hoy seta, and type error holo jodi ami kono type oparation a vul kori seta
