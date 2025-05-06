279a91b7c1483748294586100dad14c068b753a2
tanveer <tanvirbullet@gmail.com>
2025-05-06 17:58:55 +0600

    section-18 completed

---
+++ section-18/practice1.js
@@ -0,0 +1,8 @@
+// PRACTICE NUMBER 1
+/* synchonous poddhoti te code line by line execute hobe, ek line er code er folafol pawar por e
+2nd line start hobe ,er age na
+*/
+
+
+// PRACTICE NUMBER 2
+/* YES! হ্যাঁ, JavaScript হল single-threaded! */
---
+++ section-18/practice2.js
@@ -0,0 +1,65 @@
+// PRACTICE NUMBER 1
+// setTimeout(() => {
+//     console.log('I wasted 3 seconds of my life by looking at screen and doing nothing');
+// }, 3000);
+
+
+
+// PRACTICE NUMBER 2
+// let num = 131;
+// let sum = 2;
+// let intervalId = setInterval(()=>{
+//     num += sum;
+//     console.log(num);
+//     if(num>=151){
+//         clearInterval(intervalId)
+//     }
+// },1000)
+
+
+// PRACTICE NUMBER 3
+// let num =0;
+// let intervalId = setInterval(()=>{
+//     console.log('I am learning JavaScript');
+//     num++;
+//     if(num===6){
+//         clearInterval(intervalId)
+//     }
+// },2000)
+
+
+
+// PRACTICE NUMBER 4
+/* Default hishebe zero micro second dhore ney */
+
+
+// console.log(1);
+// console.log(2);
+// setTimeout(()=>{console.log('hello');
+// },2000)
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// let num =0;
+// let intervalId = setInterval(()=>{
+//     console.log(num);
+//     num ++;
+//     if(num === 7){
+//         clearInterval(intervalId)
+//     }
+// },1000)
+
+
+
+
+
+
+// let num = 0;
+// const intervalId = setInterval(()=>{
+//     num++;
+//     console.log(num);
+//     if(num === 5){
+//         clearInterval(intervalId)
+//     }
+// },1000)
