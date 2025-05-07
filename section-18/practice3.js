721bb0ca4425861b1f52fc83b7435e7918780299
tanveer <tanvirbullet@gmail.com>
2025-05-07 06:41:56 +0600

    section-18 completed

---
+++ section-18/practice3.js
@@ -0,0 +1,27 @@
+// PRACTICE NUMBER 1
+/** event loop sobsomoy call stack faka howar opekkhay thake, 
+ * jodi faka na thake tahole call stack er kaj kore, and jodi
+ * call stack khali thake tahole callback queue theke kaj niye
+ * call stack a pathay.
+ */
+
+
+
+
+// PRACTICE NUMBER 2
+/**
+ * call stack hocche Syncronous and callback holo Asyncronous
+ * call stack line by line jay, ekta ses kore arekta soro kore,
+ * but callback skip kore kaj korte pare,
+ */
+
+
+
+// PRACTICE NUMBER 3
+/**
+ * JavaScript নিজে async কাজগুলো execute করে না। 
+ * বরং, সে এই কাজগুলো ব্রাউজার বা Node.js-এর runtime environment-এ “handover” করে দেয়।
+ */
+
+
+
---
+++ section-18/practice4.js
@@ -0,0 +1,38 @@
+
+// PRACTICE NUMBER 1
+/**
+ * JavaScript নিজে একটা engine এর মধ্যে চলে। Google Chrome ও Node.js-এ ব্যবহৃত হয় V8 Engine।
+ *  এই Engine দুই ধাপে কোড চালায়:
+
+Parsing (পাঠ বিশ্লেষণ)
+কোডকে ছোট ছোট টোকেন (token) করে ব্যাখ্যা করে।
+
+Compilation (কম্পাইল)
+JavaScript এখন Just-In-Time (JIT) Compiler ব্যবহার করে।
+অর্থাৎ, কোড প্রথমে bytecode বা machine code-এ রূপান্তর করে এবং এরপর সঙ্গে সঙ্গেই রান করে।
+ */
+
+
+
+
+// PRACTICE NUMBER 2
+// JavaScript হলো একটি high-level, interpreted, এবং dynamically typed programming language
+
+
+
+
+// PRACTICE NUMBER 3
+/**
+ * | ধাপ                       | ব্যাখ্যা                                            |
+| ------------------------- | --------------------------------------------------- |
+| 1. Source Code            | তুমি যে JS কোড লেখো                                 |
+| 2. Parser                 | কোডকে বিশ্লেষণ করে AST (Abstract Syntax Tree) বানায় |
+| 3. Interpreter (Ignition) | কোডকে bytecode-এ রূপান্তর করে ও রান করে             |
+| 4. Profiler               | দেখে কোন অংশ বেশি চলছে                              |
+| 5. Compiler (TurboFan)    | বারবার চলা কোডকে optimize করে machine code বানায়    |
+| 6. Execution              | optimized machine code চালায় দ্রুত গতিতে            |
+
+ */
+
+
+// PRACTICE NUMBER 4
